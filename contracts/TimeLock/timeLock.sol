18094875af6f012d3df288b875d2b48dd3d0bcc0
한성원 <holyhansss@gmail.com>
2022-08-03 18:39:56 +0900

    timelock

--- contracts/TimeLock/timeLock.sol
+++ contracts/TimeLock/timeLock.sol
@@ -1,7 +1,127 @@
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.13;
 
 contract TimeLock {
+    error NotOwnerError();
+    error AlreadyQueuedError(bytes32 txId);
+    error TimestampNotInRangeError(uint256 blockTimestamp, uint256 timestamp);
+    error NotQueuedError(bytes32 txId);
+    error TimestampNotPassedError(uint256 blockTimestmap, uint256 timestamp);
+    error TimestampExpiredError(uint256 blockTimestamp, uint256 expiresAt);
+    error TxFailedError();
+
+    uint256 public constant MIN_DELAY = 10;
+    uint256 public constant MAX_DELAY = 1000;
+    uint256 public constant GRACE_PERIOD = 1000; // 유예 기간
+
+    address public owner;
+
+    mapping(bytes32 => bool) public queued;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    modifier onlyOwner() {
+        if (msg.sender != owner) {
+            revert NotOwnerError();
+        }
+        _;
+    }
+
+    function getTxId(
+        address _target,
+        uint _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint _timestamp
+    ) public pure returns (bytes32) {
+        return keccak256(abi.encode(_target, _value, _func, _data, _timestamp));
+    }
+
+    function queue(
+        address _target,
+        uint256 _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint256 _timestamp
+    ) external onlyOwner returns (bytes32 txId) {
+        txId = getTxId(_target, _value, _func, _data, _timestamp);
+
+        if (queued[txId]) {
+            revert AlreadyQueuedError(txId);
+        }
+
+        if ( _timestamp < block.timestamp + MIN_DELAY || _timestamp > block.timestamp + MAX_DELAY ) {
+            revert TimestampNotInRangeError(block.timestamp, _timestamp);
+        }
+
+        queued[txId] = true;
+ 
+        // emit event
+    }
+
+    function execute(
+        address _target,
+        uint256 _value,
+        string calldata _func,
+        bytes calldata _data,
+        uint256 _timestamp
+    ) external payable onlyOwner returns (bytes memory) {
+        bytes32 txId = getTxId(_target, _value, _func, _data, _timestamp);
+        if (queued[txId]) {
+            revert NotQueuedError(txId);
+        }
+
+        if (block.timestamp < _timestamp) {
+            revert TimestampNotPassedError(block.timestamp, _timestamp);
+        }
+        if (block.timestamp > _timestamp + GRACE_PERIOD) {
+            revert TimestampExpiredError(block.timestamp, _timestamp + GRACE_PERIOD);
+        }
+
+        queued[txId] = false;
+
+        bytes memory data;
+        if(bytes(_func).length > 0){
+            data = abi.encodePacked(bytes4(keccak256(bytes(_func))), _data);
+        } else {
+            data = _data;
+        }
+
+        (bool success, bytes memory res) =  _target.call{value: _value}(data);
+        if(!success) {
+            revert TxFailedError();
+        }
+
+        // emit event
+
+        return res;
+    }
+    function cancel(bytes32 _txId) external onlyOwner {
+        if(!queued[_txId]) {
+            revert NotQueuedError(_txId);
+        }
+        queued[_txId] = false;
+    }
+}
 
 
+contract timeLockTestContract {
+
+    address timeLock;
+
+    constructor(address _timeLock) {
+        timeLock = _timeLock;
+    }
+
+    function test() external {
+        require(msg.sender == timeLock);
+        // - upgrade contracdt
+        // - tranfer funds
+        // - switch price oracle
+        // ...
     
+    }
+
+
 }
